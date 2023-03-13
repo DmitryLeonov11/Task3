610ca731c1bf9ba3779c4b40781dcb6520837bbe
DmitriyLeonov <dleonov701@gmail.com>
2023-03-13 23:19:17 +0300

    key and hmac generator added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SHA3.Net;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -34,14 +36,45 @@ namespace Task3
                     }
                 }
             }
-            
-            Console.WriteLine("he he");
+            Generator generator = new Generator();
+            WinTableGenerator winTableGenerator = new WinTableGenerator();
+            Rules rules = new Rules();
+            HelpTableGenerator helpTableGenerator = new HelpTableGenerator();
+            string key = generator.Generate().Replace("-", "");
+            string HMAC = generator.GenerateHMAC(key,"hello");
+            Console.WriteLine("HMAC: " + HMAC);
+            Console.WriteLine("HMAC key: " + key);
         }
     }
 
     public class Generator
     {
+        private Sha3 sha3 = Sha3.Sha3256();
+        private RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+        public string Generate()
+        {
+            byte[] randomBytes = new byte[32];
+            randomNumberGenerator.GetBytes(randomBytes);
+            string key = BitConverter.ToString(randomBytes);
+            return key;
+        }
 
+        public string GenerateHMAC(string key, string variant)
+        {
+            Stream stream = GenerateStreamFromString(key + variant);
+            string hash = BitConverter.ToString(sha3.ComputeHash(stream)).Replace("-", "");
+            return hash;
+        }
+
+        private Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
     }
 
     public class WinTableGenerator
